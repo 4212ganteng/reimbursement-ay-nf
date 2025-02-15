50d0852394dec63cbdabbe789f9bd4dc11a0aaaa
rejosari-space <abdulazizmuslim.id@gmail.com>
2025-02-15 13:25:09 +0700

    setup s3 client and create funct upload file

---
+++ src/utils/aws.ts
@@ -0,0 +1,42 @@
+import { S3Client, ListBucketsCommand, PutObjectCommand } from '@aws-sdk/client-s3'
+
+const s3client = new S3Client({
+  region: 'apac',
+  endpoint: process.env.R2_S3API_URL,
+  credentials: {
+    accessKeyId: process.env.R2_ACCESS_ID,
+    secretAccessKey: process.env.R2_SECRET_KEY
+  }
+})
+
+interface UploadFileArgs {
+  key: string
+  folder: string
+  body: File | Uint8Array
+}
+
+export async function uploadFile(args: UploadFileArgs) {
+  try {
+    const buffer = args.body instanceof File ? Buffer.from(await args.body.arrayBuffer()) : args.body
+    const contentType = args.body instanceof File ? 'image/png' : 'application/pdf'
+
+    const data = await s3client.send(
+      new PutObjectCommand({
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: `${args.folder}/${args.key}`,
+        ContentType: contentType,
+        Body: buffer
+      })
+    )
+
+    return {
+      success: true,
+      data
+    }
+  } catch (error) {
+    return {
+      success: false,
+      error
+    }
+  }
+}
